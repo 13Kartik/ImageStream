1b642ddb4ba00514cb2277cb20210afad7158ed6
Prince Changani <princechangani56385638@gmail.com>
2024-02-14 14:20:37 +0530

    signUp page created
    Signed-off-by: Prince Changani <princechangani56385638@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,5 +10,5 @@ export const routes: Routes = [
     {
         path: 'signup',
         component: SignUpComponent
-    },
+    }
 ];
--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,56 +1,36 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, HostListener, OnDestroy, OnInit, input } from '@angular/core';
+import { AbstractControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormGroup, FormControl } from '@angular/forms';
+
+// fontawesome import
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowRightToBracket, faEnvelope, faKey, faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons';
+
+// importing custome validators
+import { passwordMatch } from '../../custome-validations/passwordMatch';
+
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [CommonModule ,ReactiveFormsModule],
+  imports: [CommonModule ,ReactiveFormsModule, FontAwesomeModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
-export class SignUpComponent implements OnInit, OnDestroy{
-  signupForm = new FormGroup(
-    {
-      firstName : new FormControl('', [Validators.required]),
-      lastName : new FormControl('', [Validators.required]),
-      email : new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$')]),
-      password : new FormControl('', [Validators.required, Validators.minLength(8)]),
-      confirmPassword : new FormControl('', [Validators.required, Validators.minLength(8)])
-    }
-  )
-  get firstNameValidation()
-  {
-    return this.signupForm.get('firstName');
-  }
-  get lastNameValidation()
-  {
-    return this.signupForm.get('lastName');
-  }
-  get emailValidation()
-  {
-    return this.signupForm.get('email');
-  }
-  get passwordValidation()
-  {
-    return this.signupForm.get('password');
-  }
-  get confirmPasswordValidation()
-  {
-    return this.signupForm.get('confirmPassword');
-  }
+export class SignUpComponent implements OnInit{
 
-  onSubmit()
-  {
-    console.log(this.signupForm.value);
+  // fontawesome icon declaration
+  loginIcon = faArrowRightToBracket;
+  mailIcon = faEnvelope;
+  passwordIcon = faKey;
+  eyeIconPassword = faEye;
+  eyeIconConfirmPassword = faEye;
 
-    let email: any = this.signupForm.value.email;
-    let password: any = this.signupForm.value.password;
-    localStorage.setItem("email", email);
-    localStorage.setItem("password", password);
-  }
-
+  // variable used in this components 
+  passwordType:string = 'password';
+  confirmPasswordType:string = 'password';
+  
   constructor() {
     console.log('Constructor called');
   }
@@ -58,9 +38,69 @@ export class SignUpComponent implements OnInit, OnDestroy{
   ngOnInit() {
     console.log('ngOnInit called');
   }
+  
+  // Validation Code
+  signupForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern('^[A-Za-z]+$')]),
+    lastName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern('^[A-Za-z]+$')]),
+    email: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$')]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    confirmPassword: new FormControl('', [Validators.required])
+  }, [passwordMatch("password", "confirmPassword")]);
+  
+  get firstNameValidation(): AbstractControl | null
+  {
+    return this.signupForm.get('firstName');
+  }
+  get lastNameValidation(): AbstractControl | null
+  {
+    return this.signupForm.get('lastName');
+  }
+  get emailValidation(): AbstractControl | null
+  {
+    return this.signupForm.get('email');
+  }
+  get passwordValidation(): AbstractControl | null
+  {
+    // console.log(this.signupForm.get('password') as FormControl);
+    return this.signupForm.get('password');
+  }
+  get confirmPasswordValidation(): AbstractControl | null
+  {
+    // console.log(this.signupForm.get('confirmPassword'));
+    // console.log(this.signupForm.get('confirmPassword') as FormControl);
+    return this.signupForm.get('confirmPassword');
+  }
 
-  ngOnDestroy() {
-    console.log('ngOnDestroy called');
-    localStorage.clear();
+  // handling from submit method
+  onSubmit()
+  {
+    console.log(this.signupForm.value);
+  }
+
+  togglePassword(inputType: string){
+    // console.log(inputType);
+    if(inputType == "password")
+    {
+      this.passwordType = "text";
+      this.eyeIconPassword = faEyeSlash;
+    }else{
+      this.passwordType = "password";
+      this.eyeIconPassword = faEye;
+    }
+  }
+  
+  toggleConfirmPassword(inputType: string)
+  {
+    // console.log("confirm",inputType);
+    if(inputType == "password")
+    {
+      this.confirmPasswordType = "text";
+      this.eyeIconConfirmPassword = faEyeSlash;
+    }
+    else{
+      this.confirmPasswordType = "password";
+      this.eyeIconConfirmPassword = faEye;
+    }
   }
 }

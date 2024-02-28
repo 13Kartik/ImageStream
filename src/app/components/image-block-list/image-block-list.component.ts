de3d4f505d181d4e240dce54af69ef2fead98ce5
Prince Changani <princechangani56385638@gmail.com>
2024-02-28 12:34:31 +0530

    Create-image Model Created
    Signed-off-by: Prince Changani <princechangani56385638@gmail.com>

--- src/app/components/image-block-list/image-block-list.component.ts
+++ src/app/components/image-block-list/image-block-list.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, inject } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 // fontawesome import
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCopy } from '@fortawesome/free-solid-svg-icons';
 
 import {ClipboardModule, Clipboard} from '@angular/cdk/clipboard'
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-image-block-list',
   standalone: true,
-  imports: [NavbarComponent, FontAwesomeModule, ClipboardModule],
+  imports: [NavbarComponent, FontAwesomeModule, ClipboardModule, RouterModule],
   templateUrl: './image-block-list.component.html',
   styleUrl: './image-block-list.component.css'
 })
 export class ImageBlockListComponent {
+
+  // injecting NgbModel
+  private modalService = inject(NgbModal);
+  
   // Variable declaration
   copyImageLink = faCopy;
 
@@ -24,4 +30,9 @@ export class ImageBlockListComponent {
     console.log("copied!!!");
     this.clipboard.copy("Image Link is copied!!!");
   }
+
+  // Fucntion for model opening
+  openVerticallyCentered(content: TemplateRef<any>) {
+		this.modalService.open(content, { centered: true });
+	}
 }
